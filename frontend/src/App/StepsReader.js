01ea04d74aa78e9202c58780be349599d7a1ef0e
Michal Konecny <mikkonecny@gmail.com>
2026-02-14 10:24:27 +0000

    delete purescript frontend, going with vue

--- frontend/src/App/StepsReader.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-export const _getNewSteps = (currLenghtRead) => async () => {
-    const stepStrings = await fetch(`http://127.0.0.1:7379/LRANGE/steps_json/${currLenghtRead}/-1`)
-        .then((resp) => resp.json())
-        .then((json) => json["LRANGE"])
-    return stepStrings;
-}
--- frontend/src/App/StepsTree.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-export const _scrollToElementIdIfNotVisible = (elementId) => async () => {
-  const elem = document.getElementById(elementId)
-  if(elem){
-    elem.scrollIntoViewIfNeeded()
-  }
-}
